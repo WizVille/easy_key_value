b734cd82522d19c08eed3483f365c8e0a7a91fbd
Intrepidd <adrien.siami@gmail.com>
2012-06-09 15:46:02 +0200

    Adding model extensions

--- lib/easy_key_value.rb
+++ lib/easy_key_value.rb
@@ -1,5 +1,7 @@
 require "easy_key_value/version"
+require "easy_key_value/model_extensions"
 
-module EasyKeyValue
-  # Your code goes here...
+if defined? ActiveRecord::Base
+  ActiveRecord::Base.send(:include, EasyKeyValue::ModelExtensions)
 end
+
---
+++ lib/easy_key_value/model_extensions.rb
@@ -0,0 +1,9 @@
+module EasyKeyValue
+  module ModelExtensions
+
+    def acts_as_key_value_store
+      puts "Acting as a key / value store"
+    end
+
+  end
+end
