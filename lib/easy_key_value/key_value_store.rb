c293d98780dcb56154d770bc89c9347f76e5a299
Intrepidd <adrien.siami@gmail.com>
2012-06-12 17:33:47 +0200

    Doing some refactoring in order to reduce SQL queries, key are loaded only once into the model in a hash that is updated during the life cycle of the object

--- lib/easy_key_value/key_value_store.rb
+++ lib/easy_key_value/key_value_store.rb
@@ -7,7 +7,12 @@ module EKV
       # @param [String] key The key
       # @param [String] value The value
       def add_key(key, value)
-        EasyKeyValue.add_key(self, key, value)
+        self.load_ekv
+        if EasyKeyValue.add_key(self, key, value) == true
+          @ekv[key] = value
+          return true
+        end
+        false
       end
 
       # Returns the value of a given key
@@ -15,9 +20,8 @@ module EKV
       # @param [String] key The key we want the value of
       # @return [String, nil] The value or nil if it does not exist
       def get_key(key)
-        ekv = EasyKeyValue.find_by_ekv_id_and_ekv_type_and_key(self.id, self.class.to_s, key)
-        return nil if ekv.nil?
-        ekv.value
+        self.load_ekv
+        @ekv[key]
       end
 
       # Removes a given key
@@ -25,10 +29,25 @@ module EKV
       # @param [String] key The key we want to destroy
       # @return [Boolean] Wether or not the key was destroyed
       def del_key(key)
+        self.load_ekv
         ekv = EasyKeyValue.find_by_ekv_id_and_ekv_type_and_key(self.id, self.class.to_s, key)
         return false if ekv.nil?
         ekv.destroy
+        @ekv.delete key
         true
+      end
+
+protected
+
+      # Loads The key and values for the current model
+      def load_ekv
+        return if defined? @ekv or !@ekv.nil?
+
+        keys = EasyKeyValue.find_all_by_ekv_id_and_ekv_type(self.id, self.class.to_s)
+        @ekv = {}
+        keys.each do |k|
+          @ekv[k.key] = k.value
+        end
       end
 
     end
