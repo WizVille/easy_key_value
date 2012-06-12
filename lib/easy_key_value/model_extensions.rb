f2d4aec6803b9147c52c2adac41e1bce39457f77
Intrepidd <adrien.siami@gmail.com>
2012-06-12 15:18:14 +0200

    Fixing the class method extension

--- lib/easy_key_value/model_extensions.rb
+++ lib/easy_key_value/model_extensions.rb
@@ -1,6 +1,13 @@
 module EasyKeyValue
   module ModelExtensions
 
+    def self.included(base)
+      base.send(:extend, ClassMethods)
+    end
+  end
+
+  module ClassMethods
+
     def acts_as_key_value_store
       puts "Acting as a key / value store"
     end
--- test/models/Foo.rb
+++ test/models/Foo.rb
@@ -3,6 +3,6 @@ require 'easy_key_value'
 
 class Foo < ActiveRecord::Base
 
-  #acts_as_key_value_store
+  acts_as_key_value_store
 
 end
